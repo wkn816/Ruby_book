235d81e0ac260484e0ceebfb6ed1d56007856ec4
wkn816 <wkn816@gmail.com>
2020-11-02 00:24:49 +0900

    p 36

--- helloruby.rb
+++ helloruby.rb
@@ -1,2 +1,12 @@
-print("楽しいrubyの始まり")
-print("hello, \"ruby\".\n")
+
+# print("楽しいrubyの始まり")
+# print("hello, \"ruby\".\n")
+# print "hello,", "ruby", ".", "n"
+
+# puts "hello,", "test"
+# puts 100
+# puts "100"
+# p 100
+# p "100"
+# p "文字化け検証"
+p 20 + 8 / 2
