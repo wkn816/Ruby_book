c4e7c165df63ca746c869d28977fc69e76b2cda7
wkn816 <wkn816@gmail.com>
2020-11-08 02:47:37 +0900

    1-11

---
+++ lesson1-11.rb
@@ -0,0 +1,14 @@
+# aの値が１０以上の時は”greater”９以下の場合は”smaller”
+a = 20
+# if a > 10 then
+#   print "greater\n"
+# end
+# if a < 10 then
+#   print "smaller\n"
+# end
+
+if a > 10 
+  print "greater\n"
+else 
+  print "smaller\n"
+end
