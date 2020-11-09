3708bbf1a47e4ba6a0d6eac67a65fb5e9a76f04f
wkn816 <wkn816@gmail.com>
2020-11-09 13:33:07 +0900

    lesson2_finish

---
+++ lesson2-2.rb
@@ -0,0 +1,9 @@
+# address = {name: "高橋",miyi: "タカハシ"}
+# address.each do |a,b|
+#   puts "#{a}: #{b}"
+# end
+
+p /Ruby/ =~ "aaa Ruby aaa"
+p /ruby/ =~ "Ruby"
+p /ruby/ =~ "ruby"
+p /Ruby/i =~ "ruby"
