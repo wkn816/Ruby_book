887342ca2f574888038e5d1a31e3bf5db030db91
wkn816 <wkn816@gmail.com>
2020-11-16 19:24:20 +0900

    def

--- lesson7-1.rb
+++ lesson7-1.rb
@@ -1,2 +1,19 @@
 # p [1, 2, 3, 4].index(1)
-p "a,b,c,d,e".split(" ")
+# p "a,b,c,d,e".split(" ")
+# def volume(x,y,z)
+#   return x*y*z
+# end
+# p volume(2,3,1)
+
+def myloop
+  while true
+    yield
+  end
+end
+
+num = 1
+myloop do
+  puts "num is #{num}"
+  break if num > 10
+  num *=2
+end
