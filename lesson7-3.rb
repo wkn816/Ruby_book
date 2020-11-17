e5bb813938f152339dcb7eda6b35c546f4350455
wkn816 <wkn816@gmail.com>
2020-11-17 10:17:07 +0900

    lesson_7

---
+++ lesson7-3.rb
@@ -0,0 +1,12 @@
+# def area(x:0, y:0, z:0)
+#   xy = x * y
+#   yz = y * z
+#   zx = z * x
+#   (xy + yz + zx) *2
+# end
+# p area(x:2, y:3, z:4)
+
+def area(x:2, y:2)
+  x * y
+end
+p area(x:4, y:5)
